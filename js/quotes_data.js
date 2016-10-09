353899c67ef5cb5ded518177688071fbfda02428
Arvid <arvid.bild@studentvikarie.se>
2016-10-09 08:53:27 +0200

    Create an array of objects

---
+++ js/quotes_data.js
@@ -0,0 +1,39 @@
+var quotes = [
+    
+    {
+    quote: 'hej jag heter kalle',
+    source: 'Arvid',
+    citation: 'bok 1',
+    year: '1997'        
+    },
+    {
+    quote: 'test2',
+    source: 'frida',
+    citation: 'bok2',
+    year: '2007'    
+    },
+    {
+    quote: 'test3',
+    source: 'melker',
+    citation:'bok3',
+    year: '2016'        
+    },
+    {
+    quote: 'test4',
+    source: 'per',
+    citation: 'bok4',
+    year: '1991'
+    },
+    {
+    quote: 'test5',
+    source: 'cina',
+    citation: 'bok5',
+    year: '1962'
+    },
+    {
+    quote: 'test6',
+    source: 'gustav',
+    citation: 'bok6',
+    year: '1956'
+    }
+];  
--- js/script.js
+++ js/script.js
@@ -2,3 +2,48 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+prompt('hej');
+
+
+
+var quotes = [
+    
+    {
+    qoute: 'hej jag heter kalle',
+    source: 'Arvid',
+    citation: 'bok 1',
+    year: 1997        
+    },
+    {
+    qoute: 'test2',
+    source: 'frida',
+    citation: 'bok2',
+    year: 2007    
+    },
+    {
+    qoute: 'test3',
+    source: 'melker',
+    citation:'bok3',
+    year: 2016        
+    },
+    {
+    qoute: 'test4',
+    source: 'per',
+    citation: 'bok4',
+    year: 1991
+    },
+    {
+    qoute: 'test5',
+    source: 'cina',
+    citation: 'bok5',
+    year: 1962
+    },
+    {
+    qoute: 'test6',
+    source: 'gustav',
+    citation: 'bok6',
+    year: 1956
+    }
+];  
+
